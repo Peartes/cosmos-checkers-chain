eb9a521592b7f1a3ccb21b7dd0f2554689c0ca9c
peartes <kenny.fale.kf@gmail.com>
2023-03-24 12:42:50 +0100

    chore: record game winner

--- go.mod
+++ go.mod
@@ -6,12 +6,10 @@ require (
 	github.com/cosmos/cosmos-sdk v0.45.4
 	github.com/cosmos/ibc-go/v3 v3.0.0
 	github.com/gogo/protobuf v1.3.3
-	github.com/golang/protobuf v1.5.2
+	github.com/golang/protobuf v1.5.3
 	github.com/gorilla/mux v1.8.0
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.2 // indirect
 	github.com/ignite-hq/cli v0.22.0
-	github.com/regen-network/cosmos-proto v0.3.1 // indirect
 	github.com/spf13/cast v1.4.1
 	github.com/spf13/cobra v1.4.0
 	github.com/stretchr/testify v1.8.1
--- vue/src/store/generated/alice/checkers/alice.checkers.checkers/module/index.ts
+++ vue/src/store/generated/alice/checkers/alice.checkers.checkers/module/index.ts
@@ -4,15 +4,15 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
-import { MsgCreateGame } from "./types/checkers/tx";
-import { MsgPlayGame } from "./types/checkers/tx";
 import { MsgRejectGame } from "./types/checkers/tx";
+import { MsgPlayGame } from "./types/checkers/tx";
+import { MsgCreateGame } from "./types/checkers/tx";
 
 
 const types = [
-  ["/alice.checkers.checkers.MsgCreateGame", MsgCreateGame],
-  ["/alice.checkers.checkers.MsgPlayGame", MsgPlayGame],
   ["/alice.checkers.checkers.MsgRejectGame", MsgRejectGame],
+  ["/alice.checkers.checkers.MsgPlayGame", MsgPlayGame],
+  ["/alice.checkers.checkers.MsgCreateGame", MsgCreateGame],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -45,9 +45,9 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
-    msgCreateGame: (data: MsgCreateGame): EncodeObject => ({ typeUrl: "/alice.checkers.checkers.MsgCreateGame", value: MsgCreateGame.fromPartial( data ) }),
-    msgPlayGame: (data: MsgPlayGame): EncodeObject => ({ typeUrl: "/alice.checkers.checkers.MsgPlayGame", value: MsgPlayGame.fromPartial( data ) }),
     msgRejectGame: (data: MsgRejectGame): EncodeObject => ({ typeUrl: "/alice.checkers.checkers.MsgRejectGame", value: MsgRejectGame.fromPartial( data ) }),
+    msgPlayGame: (data: MsgPlayGame): EncodeObject => ({ typeUrl: "/alice.checkers.checkers.MsgPlayGame", value: MsgPlayGame.fromPartial( data ) }),
+    msgCreateGame: (data: MsgCreateGame): EncodeObject => ({ typeUrl: "/alice.checkers.checkers.MsgCreateGame", value: MsgCreateGame.fromPartial( data ) }),
     
   };
 };
